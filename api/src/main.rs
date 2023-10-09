8076ff7591e79ee4755592a8de1fc20424b6ef61
Daan Kuik <dkuik@sogyo.nl>
2023-10-09 16:40:44 +0200

    Api and domain linked

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use domain;
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/test")]
+fn test() -> &'static str {
+    domain::get_str()
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/api", routes![test])
 }
--- domain/src/lib.rs
+++ domain/src/lib.rs
@@ -2,6 +2,10 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn get_str() -> &'static str {
+    "This string came from the domain"
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
