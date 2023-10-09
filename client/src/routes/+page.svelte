f9626fed5873f62a3e497517e3bfd8f56313a5f5
Daan Kuik <dkuik@sogyo.nl>
2023-10-09 16:41:00 +0200

    button sends API request

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,4 +1,17 @@
-<button>Click me!</button>
+<script lang='ts'>
+  let information: string;
+
+  async function handleClick() {
+    let response = await fetch("api/test");
+    information = await response.text();
+  };
+</script>
+
+<button on:click={ handleClick }>Click me!</button>
+
+{#if information}
+  <p>{information}</p>
+{/if}
 
 <style>
   button {
