65ef8828d715cebb2856b3211f6e516414cd1c00
Daan Kuik <dkuik@sogyo.nl>
2023-10-11 12:32:33 +0200

    datastore now uses a mutex around the datapoint vector

--- domain/src/datastore.rs
+++ domain/src/datastore.rs
@@ -1,19 +1,21 @@
 use crate::datapoint::{create_datapoint, Datapoint};
+use std::sync::Mutex;
 
-struct Datastore {
-    datapoints: Vec<Datapoint>,
+pub struct Datastore {
+    datapoints: Mutex<Vec<Datapoint>>,
 }
 
 impl Datastore {
     pub fn new() -> Datastore {
         Datastore {
-            datapoints: Vec::new(),
+            datapoints: Mutex::new(Vec::new()),
         }
     }
 
-    pub fn add_datapoint(&mut self, input: &str) -> () {
+    pub fn add_datapoint(&self, input: &str) -> () {
         let datapoint = create_datapoint(input);
-        self.datapoints.push(datapoint);
+        let mut lock = self.datapoints.lock().expect("mutex holder crashed");
+        lock.push(datapoint);
     }
 }
 
@@ -23,10 +25,11 @@ mod tests {
 
     #[test]
     fn datapoints_are_stored_in_datastore_after_add_function() {
-        let mut datastore = Datastore::new();
+        let datastore = Datastore::new();
 
         datastore.add_datapoint("Some text with +some +tags");
+        let lock = datastore.datapoints.lock().expect("mutex holder crashed");
 
-        assert_eq!(1, datastore.datapoints.len());
+        assert_eq!(1, lock.len());
     }
 }
