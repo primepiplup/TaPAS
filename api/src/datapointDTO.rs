b0bb8010ed5a5175edcef009df4e71ad56e9c335
Daan Kuik <dkuik@sogyo.nl>
2023-10-11 15:33:43 +0200

    datapointDTO now includes timestamp string

--- api/src/datapointDTO.rs
+++ api/src/datapointDTO.rs
@@ -1,15 +1,19 @@
+use chrono::prelude::*;
 use domain::datapoint::Datapoint;
 use rocket::serde::Serialize;
 
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
 pub struct DatapointDTO {
+    timestamp: String,
     data: String,
     tags: Vec<String>,
 }
 
 pub fn from(datapoint: Datapoint) -> DatapointDTO {
+    let datapoint_timestamp = datapoint.get_datetime().to_rfc2822();
     DatapointDTO {
+        timestamp: datapoint_timestamp,
         data: datapoint.get_data().to_owned(),
         tags: datapoint.get_tags().to_owned(),
     }
