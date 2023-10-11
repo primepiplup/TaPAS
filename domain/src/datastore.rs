90678309a736cd2b275c63674aad197c758f6ae6
Daan Kuik <dkuik@sogyo.nl>
2023-10-11 14:55:02 +0200

    retrieve copy of all stored datapoints

--- domain/src/datastore.rs
+++ domain/src/datastore.rs
@@ -17,10 +17,17 @@ impl Datastore {
         let mut lock = self.datapoints.lock().expect("mutex holder crashed");
         lock.push(datapoint);
     }
+
+    pub fn retrieve_datapoints(&self) -> Vec<Datapoint> {
+        let lock = self.datapoints.lock().expect("mutex holder crashed");
+        lock.clone()
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::datapoint;
+
     use super::*;
 
     #[test]
@@ -31,5 +38,18 @@ mod tests {
         let lock = datastore.datapoints.lock().expect("mutex holder crashed");
 
         assert_eq!(1, lock.len());
+    }
+
+    #[test]
+    fn datapoints_can_be_retrieved_from_datastore() {
+        let datastore = Datastore::new();
+        let input_text = "Some text with +some +tags";
+        let expected_datapoint = datapoint::create_datapoint(input_text);
+
+        datastore.add_datapoint(input_text);
+        let actual_datapoint = &datastore.retrieve_datapoints()[0];
+
+        assert!(actual_datapoint.data_same_as(&expected_datapoint));
+        assert!(actual_datapoint.tags_same_as(&expected_datapoint));
     }
 }
