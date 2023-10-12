6a0fbca272dcda6afc203d0346d1fb6e228d631c
Daan Kuik <dkuik@sogyo.nl>
2023-10-12 17:22:47 +0200

    Vite now serves another proxy address for images

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,6 +10,12 @@ export default defineConfig({
 				changeOrigin: true,
 			 	secure: false,
 				ws: true
+			},
+			'/plot': {
+				target: 'http://localhost:8000',
+				changeOrigin: true,
+			 	secure: false,
+				ws: true
 			}
 		}
 	}
