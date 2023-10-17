b44245fa1f2cabfad57d92038f2cb2dd352fdbcf
Daan Kuik <dkuik@sogyo.nl>
2023-10-17 16:43:12 +0200

    Reusable error component

---
+++ client/src/routes/error.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let errorText = "";
+</script>
+
+<p class="error"><span>{errorText}</span></p>
+
+<style>
+  .error {
+    color: #C1292E;
+    padding: 10px;
+    border: 2px solid #C1292E;
+    background-color: #D1AC00;
+    font-weight: bold;
+    width: fit-content;
+    text-align: center;
+  }
+</style>
