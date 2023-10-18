97f7ec2eee0b8c1ad5905e409b883055092047fd
Daan Kuik <dkuik@sogyo.nl>
2023-10-18 10:54:32 +0200

    separated plotcolors theme object into file

--- domain/src/plotter.rs
+++ domain/src/plotter.rs
@@ -1,52 +1,8 @@
-use std::io::Error;
-
 use crate::datapoint::{create_datapoint, Datapoint};
+use crate::plotcolors::PlotColors;
 use chrono::{DateTime, Local, TimeZone};
 use plotters::prelude::*;
-
-struct PlotColors {
-    background: RGBColor,
-    textcolor: RGBColor,
-    highlight: RGBColor,
-    darklight: RGBColor,
-    labelcolor: ShapeStyle,
-}
-
-impl PlotColors {
-    fn new() -> PlotColors {
-        PlotColors {
-            background: RGBColor(12, 22, 24),
-            textcolor: RGBColor(209, 172, 0),
-            highlight: RGBColor(250, 244, 211),
-            darklight: RGBColor(60, 73, 76),
-            labelcolor: ShapeStyle {
-                color: RGBAColor(193, 41, 46, 1.0),
-                filled: true,
-                stroke_width: 2,
-            },
-        }
-    }
-
-    pub fn background(&self) -> &RGBColor {
-        &self.background
-    }
-
-    pub fn textcolor(&self) -> &RGBColor {
-        &self.textcolor
-    }
-
-    pub fn highlight(&self) -> &RGBColor {
-        &self.highlight
-    }
-
-    pub fn darklight(&self) -> &RGBColor {
-        &self.darklight
-    }
-
-    pub fn labelstyle(&self) -> &ShapeStyle {
-        &self.labelcolor
-    }
-}
+use std::io::Error;
 
 pub fn basic_plot(
     data: &Vec<Datapoint>,
