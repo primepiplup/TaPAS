407cdd61316dd3e05df9a93351521752da94217a
Daan Kuik <dkuik@sogyo.nl>
2023-10-26 12:00:38 +0200

    DatapointDTO now has (explicit) From<Datapoint> implementation

--- api/src/datapoint_dto.rs
+++ api/src/datapoint_dto.rs
@@ -7,21 +7,25 @@ pub struct DatapointDTO {
     timestamp: String,
     data: String,
     tags: Vec<String>,
+    key: u64,
 }
 
-pub fn from(datapoint: Datapoint) -> DatapointDTO {
-    let datapoint_timestamp = datapoint.get_datetime().to_rfc2822();
-    DatapointDTO {
-        timestamp: datapoint_timestamp,
-        data: datapoint.get_data().to_owned(),
-        tags: datapoint.get_tags().to_owned(),
+impl From<Datapoint> for DatapointDTO {
+    fn from(datapoint: Datapoint) -> DatapointDTO {
+        let datapoint_timestamp = datapoint.get_datetime().to_rfc2822();
+        DatapointDTO {
+            timestamp: datapoint_timestamp,
+            data: datapoint.get_data().to_owned(),
+            tags: datapoint.get_tags().to_owned(),
+            key: datapoint.get_key(),
+        }
     }
 }
 
 pub fn dto_vec_from(datapoints: Vec<Datapoint>) -> Vec<DatapointDTO> {
     let mut collector = Vec::new();
     for datapoint in datapoints {
-        collector.push(from(datapoint));
+        collector.push(DatapointDTO::from(datapoint));
     }
     collector
 }
