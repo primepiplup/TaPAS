1d2ef81dae8f2eab3a8334fcc862fa664f624b9a
Daan Kuik <dkuik@sogyo.nl>
2023-10-31 14:18:44 +0100

    Summary is now displayed as elements of a summary table

---
+++ client/src/routes/compare/summary.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let summary: {name: string, mean: number, p: number}
+</script>
+
+<tr>
+	<td>{summary.name}</td>
+	<td>{summary.mean}</td>
+	<td>{summary.p}</td>
+</tr>
+
+<style>
+</style>
