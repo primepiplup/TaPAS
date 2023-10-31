a5088ede3c8072728ee85526b1b5e8d162c4095b
Daan Kuik <dkuik@sogyo.nl>
2023-10-31 16:16:18 +0100

    ParsedQuery title generation takes excluded tags into account

--- domain/src/parsedquery.rs
+++ domain/src/parsedquery.rs
@@ -19,7 +19,16 @@ impl ParsedQuery {
         self.query
             .clone()
             .into_iter()
-            .map(|elem| elem[0].clone())
+            .map(|elem| {
+                if elem.len() > 1 {
+                    match elem[1].as_str() {
+                        "exclude" => format!("without {}", elem[0]),
+                        _ => elem[0].clone(),
+                    }
+                } else {
+                    elem[0].clone()
+                }
+            })
             .collect::<Vec<String>>()
             .join(", ")
     }
@@ -28,14 +37,28 @@ impl ParsedQuery {
         self.query.len() < 1 || self.query[0][0] == ""
     }
 
-    pub fn can_all_be_found_in(&self, tags: &Vec<String>) -> bool {
-        let truthvalues: Vec<bool> = self
-            .query
-            .clone()
+    pub fn can_all_be_found_in(&self, datapoint_tags: &Vec<String>) -> bool {
+        let searchtags: Vec<String> = self.without_excluded_tags();
+        let truthvalues: Vec<bool> = searchtags
             .into_iter()
-            .map(|tag| tags.contains(&tag[0]))
+            .map(|tag| datapoint_tags.contains(&tag))
             .collect();
         !truthvalues.contains(&false)
+    }
+
+    fn without_excluded_tags(&self) -> Vec<String> {
+        let mut collector: Vec<String> = Vec::new();
+        for tag in self.query.clone() {
+            if tag.len() > 1 {
+                match tag[1].as_str() {
+                    "exclude" => continue,
+                    _ => collector.push(tag[0].clone()),
+                };
+            } else {
+                collector.push(tag[0].clone());
+            }
+        }
+        return collector;
     }
 
     pub fn get_raw_parsed(&self) -> Vec<Vec<String>> {
@@ -67,5 +90,19 @@ mod tests {
         let title = parsed.generate_plot_title();
 
         assert_eq!(title, "Plot for: something, tag, else")
+    }
+
+    #[test]
+    fn generate_plot_title_sees_exclude_command_and_changes_title_appropriately() {
+        let parsed = vec![
+            vec!["something".to_string(), "exclude".to_string()],
+            vec!["tag".to_string()],
+            vec!["else".to_string()],
+        ];
+        let parsed = ParsedQuery::from(parsed);
+
+        let title = parsed.generate_plot_title();
+
+        assert_eq!(title, "Plot for: without something, tag, else")
     }
 }
